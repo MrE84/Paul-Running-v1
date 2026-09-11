e9e02935a120ae69d7024dade2c2155bedf8af96
MrE84 <135212999+MrE84@users.noreply.github.com>
2026-09-11 11:30:56 +0100

    PAU-14 export Fenix 5 validation pack

--- lib/integrations/index.ts
+++ lib/integrations/index.ts
@@ -2,6 +2,7 @@ export * from "./contracts";
 export * from "./coordinator";
 export * from "./state";
 export * from "./sync-window";
+export * from "./fenix5-validation";
 export * from "./intervals-icu/client";
 export * from "./intervals-icu/connector";
 export * from "./intervals-icu/translation";
