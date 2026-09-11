5bf7820c9db6b1c5e1e76248269566b546b75e54
MrE84 <135212999+MrE84@users.noreply.github.com>
2026-09-11 11:37:21 +0100

    PAU-14 export live validation publisher

--- lib/integrations/index.ts
+++ lib/integrations/index.ts
@@ -3,6 +3,7 @@ export * from "./coordinator";
 export * from "./state";
 export * from "./sync-window";
 export * from "./fenix5-validation";
+export * from "./fenix5-publisher";
 export * from "./intervals-icu/client";
 export * from "./intervals-icu/connector";
 export * from "./intervals-icu/translation";
